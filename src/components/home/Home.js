6ea7c4be905777360761b92befa7c18169a69153
Matt <kimchiadobo@gmail.com>
2019-06-18 18:30:15 -0400

    removing uneccessary folder

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 function Home() {
+  // sample axios call to the backend
+  axios.get('http://localhost:8080/').then(res => {
+    console.log(res.data);
+  });
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="App">
+      <header className="App-header" />
     </div>
   );
 }
