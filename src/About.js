f0d04df8d614fe6eaa277be42b1743c49a13f2f2
Matt <kimchiadobo@gmail.com>
2019-06-18 16:20:00 -0400

    creating link back to main page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
     <div>
       <h3>Welcome to the app</h3>
+      <Link to="/">Back</Link>
     </div>
   );
 }
