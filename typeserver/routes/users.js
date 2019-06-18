f89e152450a6d9179f6ac6045e2f5f5501ad649f
Matt <kimchiadobo@gmail.com>
2019-06-18 15:52:38 -0400

    adding sample MongoDB code and .env file

--- typeserver/routes/users.js
+++ typeserver/routes/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const User = require('./../model/User');
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Sending something' });
+});
+
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+
+  new User({ email, password })
+    .save()
+    .then(user => res.status(200).json({ user }))
+    .catch(e => res.status(400).json({ error: e.message }));
 });
 
 module.exports = router;
