bf723bce08184ccebd67d625f212ab81dac7c759
Matt <kimchiadobo@gmail.com>
2019-06-19 17:20:40 -0400

    adding date to game schema

--- typeserver/model/Game.js
+++ typeserver/model/Game.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose.Schema;
 
 const GameSchema = new Schema({
-  stats: [{ type: Schema.Types.ObjectId, ref: 'GameStat' }]
+  stats: [{ type: Schema.Types.ObjectId, ref: 'GameStat' }],
+  playedAt: Number
 });
 
 const Game = mongoose.model('Game', GameSchema);
