323d6a467c104d2549400b3ae7cc14affd4953aa
Matt <kimchiadobo@gmail.com>
2019-06-21 12:59:39 -0400

    adding logic to find a user in the database

--- typeserver/routes/user/user.controller.js
+++ typeserver/routes/user/user.controller.js
@@ -6,6 +6,16 @@ function index(req, res) {
   });
 }
 
+function getUser(req, res) {
+  const { email, password } = req.body;
+
+  User.findOne({ email, password }).then(foundUser => {
+    res
+      .status(200)
+      .json({ success: true, email: foundUser.email, name: foundUser.name });
+  });
+}
+
 function addUser(req, res) {
   const { name, email, password } = req.body;
 
@@ -16,5 +26,6 @@ function addUser(req, res) {
 
 module.exports = {
   index,
+  getUser,
   addUser
 };
