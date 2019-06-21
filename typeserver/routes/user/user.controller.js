4cee5d53af916e0a7af96872ec44e7399764d7db
Matt <kimchiadobo@gmail.com>
2019-06-21 13:04:39 -0400

    building structure for auth routes

--- typeserver/routes/user/user.controller.js
+++ typeserver/routes/user/user.controller.js
@@ -6,16 +6,6 @@ function index(req, res) {
   });
 }
 
-function getUser(req, res) {
-  const { email, password } = req.body;
-
-  User.findOne({ email, password }).then(foundUser => {
-    res
-      .status(200)
-      .json({ success: true, email: foundUser.email, name: foundUser.name });
-  });
-}
-
 function addUser(req, res) {
   const { name, email, password } = req.body;
 
@@ -26,6 +16,5 @@ function addUser(req, res) {
 
 module.exports = {
   index,
-  getUser,
   addUser
 };
