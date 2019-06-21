9b1fd279905b90654032c380e6bbfdd8363428f5
Matt <kimchiadobo@gmail.com>
2019-06-21 13:09:37 -0400

    building routing for login logic

--- typeserver/routes/auth/auth.controller.js
+++ typeserver/routes/auth/auth.controller.js
@@ -1,8 +1,16 @@
+const User = require('./../../db/model/User');
+
 function index(req, res) {
   res.status(200).json({
     message: 'welcome to the auth routes'
   });
 }
+
+// function login(req, res) {
+//   const { email, password } = req.body;
+
+//   User.findOne({ email, password });
+// }
 
 module.exports = {
   index
