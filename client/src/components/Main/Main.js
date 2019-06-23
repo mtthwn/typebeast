b4f67710c7052b02b953141cbbc8e2fa0e2c3b7b
Matt <kimchiadobo@gmail.com>
2019-06-23 16:36:41 -0400

    importing playnow in main

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CarSlider from './../CarSlider/CarSlider';
 // import SocialMedia from './SocialMedia';
-// import PlayNow from './PlayNow';
+import PlayNow from './../PlayNow/PlayNow';
 
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
@@ -10,6 +10,7 @@ class MainPage extends Component {
     return (
       <div>
         <CarSlider />
+        <PlayNow />
       </div>
     );
   }
