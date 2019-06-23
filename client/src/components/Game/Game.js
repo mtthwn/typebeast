ba5858b8c77aea5a9607ccce572f1387a8b9530a
JSKwok <justin.s.kwok@gmail.com>
2019-06-23 19:00:35 -0400

    Socket functionality

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Game.scss';
 import DisplayQuote from './GameUI/GameUI';
 import Background from './Background/Background';
+import socketIOClient from "socket.io-client";
 
 export default class PlayGame extends Component {
   constructor() {
@@ -16,13 +17,90 @@ export default class PlayGame extends Component {
       char: 0,
       sec: 0,
       carPositioning: 0,
+      timer: 0,
       timerStart: false,
       timerFinished: false,
-      finishLine: false
+      finishLine: false,
+      // Socket related properties:
+      endpoint: "http://127.0.0.1:8080",
+      playerCount: 0,
+      gameStart: false,
+      playerProgress: {
+        progress: 0,
+        wpm: 0
+      }
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    // Deconstruct this.state.endpoint
+    const {endpoint} = this.state;
+    // Connect to the socket
+    const socket = socketIOClient(endpoint);
+    this.setState({
+      socket
+    })
+
+    socket.on('welcome', (message) => {
+      console.log(message.description)
+      // Display welcome message. This should render so I want to set the state
+    })
+
+    socket.on('new-user-join', (message) => {
+      console.log(message.description)
+      // Display join message, this should also set state
+    })
+
+    socket.on('game-start', (message) => {
+      console.log(message.description)
+      // Display message saying game will start soon
+
+      // Set a countdown and render the typing content
+      let timerCount = 5
+      let that = this
+
+      function countdown () {
+        setTimeout( () => {
+          if (timerCount === 0) {
+            console.log("Game start.")
+            // finished = true;
+            timerStuff()
+            // return true;
+          } else {
+            console.log(timerCount)
+            timerCount--
+            countdown();
+          }
+        }, 1000);
+      };
+
+
+      function timerStuff() {
+      console.log("START")
+        that.onStartTimer();
+        that.setState({ timerStart: true });
+        that.interval = setInterval(() => {
+          that.setState(prevProps => {
+            return { sec: prevProps.sec + 1, timer: prevProps.timer + 1};
+          });
+        }, 1000);
+      }
+
+
+      countdown();
+
+      // this.onFinishTimer(value);
+
+    })
+
+    socket.on('progress-broadcast', (message) => {
+      console.log(message)
+    })
+
+    socket.on('player-left', (message) => {
+      console.log(message.description)
+    })
+  }
 
   render() {
     return (
