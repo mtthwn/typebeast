eeff116961bc28ffc3b99f7bafdafb211b8a452b
Matt <kimchiadobo@gmail.com>
2019-06-23 16:32:21 -0400

    adding playnow js file

---
+++ client/src/components/PlayNow/PlayNow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './PlayNow.scss';
+import ferarriSideView from '../../images/ferarri-side.png';
+
+const PlayNow = () => {
+  return (
+    <div className="PlayNow-container">
+      <div className="PlayNow-content">
+        <div className="PlayNow-current">
+          <h5>Car</h5>
+          <p>Lamborghini Aventador</p>
+        </div>
+        <div className="PlayNow-specs">
+          <h5>Specs</h5>
+          <p>Max Speed: 300km/h Accerelation: 100km/s</p>
+        </div>
+        <img src={ferarriSideView} alt="ferrari side view" />
+      </div>
+      <Button className="PlayNow-btn" variant="outline-light">
+        Race as Guest
+      </Button>
+    </div>
+  );
+};
+
+export default PlayNow;
