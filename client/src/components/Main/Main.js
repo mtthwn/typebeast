7aa08fbfa45df77a6bc503e55b844dfa20f6a997
Matt <kimchiadobo@gmail.com>
2019-06-23 16:50:49 -0400

    fixing packages

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CarSlider from './../CarSlider/CarSlider';
-// import SocialMedia from './SocialMedia';
+import SocialMedia from './../SocialMedia/SocialMedia';
 import PlayNow from './../PlayNow/PlayNow';
 
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class MainPage extends Component {
   render() {
     return (
       <div>
-        <CarSlider />
-        <PlayNow />
+        <Router>
+          <div>
+            <CarSlider />
+            <SocialMedia />
+            <PlayNow />
+          </div>
+        </Router>
       </div>
     );
   }
