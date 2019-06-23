5ea5f4f42bcaa308af3fd8d236ff5069c3343424
Matt <kimchiadobo@gmail.com>
2019-06-23 16:15:21 -0400

    importing header from dumb-racer

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './home/Home';
 import About from './about/About';
+import Header from './Header/Header';
 
 function Root() {
   return (
     <Router>
+      <Header />
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
     </Router>
