39992037e79893b5ae2d74a7b265dac7ade076b5
Matt <kimchiadobo@gmail.com>
2019-06-23 16:23:55 -0400

    adding Main page

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import CarSlider from './CarSlider';
+// import SocialMedia from './SocialMedia';
+// import PlayNow from './PlayNow';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
