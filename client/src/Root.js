2ce2df98274c095132b9559b6565a656a7683a4f
Matt <kimchiadobo@gmail.com>
2019-06-25 07:41:31 -0400

    removing about link

--- client/src/Root.js
+++ client/src/Root.js
@@ -3,7 +3,6 @@ import './Root.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Main from './containers/Main';
-import About from './components/about/About';
 import Header from './components/Header/Header';
 import Game from './containers/Play';
 
@@ -12,7 +11,6 @@ function Root() {
     <Router>
       <Header />
       <Route path="/" exact component={Main} />
-      <Route path="/about" component={About} />
       <Route path='/play' component={Game} />
     </Router>
   );
