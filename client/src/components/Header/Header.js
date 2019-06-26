2d6ef0d58e10d8f526c865c7cb444a091ce0861d
Daniel <nhanxtrand@gmail.com>
2019-06-26 11:00:01 +0000

    Add responsive toggle to header menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,39 +1,42 @@
-import React from "react";
-import { Nav, Navbar, Button, Form, FormControl } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "./Header.scss";
+import React from 'react';
+import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => {
   return (
     <div>
-      <Navbar className="Navbar" variant="dark">
+      <Navbar collapseOnSelect expand="lg" className="Navbar" variant="dark">
         <Navbar.Brand className="Navbar-brand">
           <Link to="/" className="nav-link">
             Typebeast
           </Link>
         </Navbar.Brand>
-        <Nav className="NavLinks mr-auto">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          <Link to="/" className="nav-link">
-            Garage
-          </Link>
-          <Link to="/" className="nav-link">
-            Shop
-          </Link>
-          <Link to="/play" className="nav-link">
-            Play Now
-          </Link>
-        </Nav>
-        <Form inline className="Header-signup">
-          <Button className="sign-up" variant="outline-light">
-            Signup
-          </Button>
-          <Button className="login" variant="outline-light">
-            Login
-          </Button>
-        </Form>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="NavLinks mr-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/" className="nav-link">
+              Garage
+            </Link>
+            <Link to="/" className="nav-link">
+              Shop
+            </Link>
+            <Link to="/play" className="nav-link">
+              Play Now
+            </Link>
+          </Nav>
+          <Form inline className="Header-signup">
+            <Button className="sign-up" variant="outline-light">
+              Signup
+            </Button>
+            <Button className="login" variant="outline-light">
+              Login
+            </Button>
+          </Form>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
