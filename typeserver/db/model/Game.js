369589197c16689bf5a90a5476e62272f0df36f6
Matt <kimchiadobo@gmail.com>
2019-06-26 16:14:56 -0400

    adding quotes to game object

--- typeserver/db/model/Game.js
+++ typeserver/db/model/Game.js
@@ -4,7 +4,11 @@ const { Schema } = mongoose;
 
 const GameSchema = new Schema({
   stats: [{ type: Schema.Types.ObjectId, ref: 'GameStat' }],
-  playedAt: Number
+  playedAt: Number,
+  quote: {
+    type: String,
+    required: true
+  }
 });
 
 const Game = mongoose.model('Game', GameSchema);
