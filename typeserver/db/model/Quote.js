23b2b685f281f4da8e9924c2d17cf50d1a87d9df
Matt <kimchiadobo@gmail.com>
2019-06-26 13:42:10 -0400

    adding quotes model and route for adding and getting quotes from db

---
+++ typeserver/db/model/Quote.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// const { Schema } = mongoose.Schema;
+
+const QuoteSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true
+  },
+  quote: {
+    type: String,
+    required: true
+  }
+});
+
+const Quote = mongoose.model('Quote', QuoteSchema);
+
+module.exports = Quote;
--- typeserver/routes/quote/index.js
+++ typeserver/routes/quote/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const Quote = require('./../../db/model/Quote')
+
 router.get('/', (req, res) => {
-    res.status(200).json({ message: 'hello' })
+
+    Quote.find({}).then(quotes => {
+        const index = Math.floor(Math.random() * quotes.length);
+
+        console.log(index); 
+        res.status(200).json({ data: quotes[index] })
+    })
+})
+
+router.post('/', (req, res) => {
+    const { author, quote } = req.body;
+
+    new Quote({ author, quote }).save().then(quote => {
+        res.status(200).json({ quote })
+    })
 })
 
 module.exports = router;
