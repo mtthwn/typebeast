4e979431c1d7c6d1c197c3801c21a547d4a4aacd
Matt <myan5150@gmail.com>
2019-06-27 18:30:12 -0400

    refactoring condition rendering of button component

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default props => (
-  <Button variant={props.variant}>
-    <Link to={props.link}>{props.text}</Link>
-  </Button>
-);
+export default props => {
+  const PageLink =
+    props.link === '/play' ? (
+      <a href="/play">{props.text}</a>
+    ) : (
+      <Link to={props.link}>{props.text}</Link>
+    );
+
+  return <Button variant={props.variant}>{PageLink}</Button>;
+};
