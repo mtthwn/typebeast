daf11470b1ae9ebfe04912ecb0590fe1197f4242
Matt <myan5150@gmail.com>
2019-06-27 18:10:40 -0400

    adding button component file

---
+++ client/src/components/Button/EndGameButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+
+export default props => (
+    <Row>
+        <Col>
+            <Button>Hello</Button>
+        </Col>
+    </Row>
+)
