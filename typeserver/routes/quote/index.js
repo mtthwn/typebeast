4a564a4ead50a1b57e808fb556a19d474beffdf3
Matt <myan5150@gmail.com>
2019-06-29 19:13:13 -0400

    removing model import in view

--- typeserver/routes/quote/index.js
+++ typeserver/routes/quote/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const Quote = require('./../../db/model/Quote');
-
 const controller = require('./quote.controller');
 
 router.get('/', controller.getQuote);
-
 router.post('/', controller.addQuote);
 
 module.exports = router;
