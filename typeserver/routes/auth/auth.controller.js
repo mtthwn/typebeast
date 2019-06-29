b71a1955db2bf67ff8213b5af9c7e4fed1121c80
Matt <myan5150@gmail.com>
2019-06-29 13:26:15 -0400

    moving routes to controller

--- typeserver/routes/auth/auth.controller.js
+++ typeserver/routes/auth/auth.controller.js
@@ -1,17 +1,99 @@
-const User = require('./../../db/model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-function index(req, res) {
+const User = require('./../../db/model/User');
+const { generateToken, getCleanUser } = require('./../../utils/auth');
+
+const index = (req, res) => {
   res.status(200).json({
     message: 'welcome to the auth routes'
   });
-}
+};
 
-// function login(req, res) {
-//   const { email, password } = req.body;
+const register = (req, res, next) => {
+  const username = req.body.username.trim();
+  const email = req.body.email.trim();
+  const password = bcrypt.hashSync(req.body.password.trim(), 10);
 
-//   User.findOne({ email, password });
-// }
+  User({ username, email, password })
+    .save((err, savedUser) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: err.message });
+      }
+
+      const token = generateToken(savedUser);
+      const user = getCleanUser(savedUser);
+
+      res.status(200).json({ success: true, user, token });
+    })
+    .catch(e => {
+      res.status(401).json({ success: false, error: e.message });
+    });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).exec((err, foundUser) => {
+    if (err) {
+      return res.status(401).json({ success: false, message: err.message });
+    }
+
+    if (!foundUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Incorrect username or password' });
+    }
+
+    bcrypt.compare(password, foundUser.password, (err, valid) => {
+      if (!valid) {
+        return res.status(404).json({
+          success: false,
+          message: 'Incorrect username or password'
+        });
+      }
+
+      const token = generateToken(foundUser);
+      const user = getCleanUser(foundUser);
+
+      res.status(200).json({ success: true, token, user });
+    });
+  });
+};
+
+const checkToken = (req, res, next) => {
+  const { token } = req.body || req.query;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: 'No token was sent' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, foundUser) => {
+    if (err) {
+      return res.status(401).json({ success: false, message: err.message });
+    }
+
+    User.findById({ _id: user._id }, (err, foundUser) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: err.message });
+      }
+
+      const user = getCleanUser(foundUser);
+
+      res.status(200).json({
+        success: true,
+        user,
+        token
+      });
+    });
+  });
+};
 
 module.exports = {
-  index
+  index,
+  register,
+  login,
+  checkToken
 };
--- typeserver/routes/auth/index.js
+++ typeserver/routes/auth/index.js
@@ -1,75 +1,12 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const router = express.Router();
-const controller = require('./auth.controller');
-const { generateToken, getCleanUser } = require('./../../utils/auth');
 
-const User = require('./../../db/model/User');
+const controller = require('./auth.controller');
 
 router.get('/', controller.index);
-
 router.post('/register', controller.register);
-
-router.post('/login', (req, res, next) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email }).exec((err, foundUser) => {
-    if (err) {
-      return res.status(401).json({ success: false, message: err.message });
-    }
-
-    if (!foundUser) {
-      return res
-        .status(404)
-        .json({ success: false, message: 'Incorrect username or password' });
-    }
-
-    bcrypt.compare(password, foundUser.password, (err, valid) => {
-      if (!valid) {
-        return res.status(404).json({
-          success: false,
-          message: 'Incorrect username or password'
-        });
-      }
-
-      const token = generateToken(foundUser);
-      const user = getCleanUser(foundUser);
-
-      res.status(200).json({ success: true, token, user });
-    });
-  });
-});
-
-router.get('/me/from/token', (req, res, next) => {
-  const { token } = req.body || req.query;
-
-  if (!token) {
-    return res
-      .status(401)
-      .json({ success: false, message: 'No token was sent' });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, foundUser) => {
-    if (err) {
-      return res.status(401).json({ success: false, message: err.message });
-    }
-
-    User.findById({ _id: user._id }, (err, foundUser) => {
-      if (err) {
-        return res.status(401).json({ success: false, message: err.message });
-      }
-
-      const user = getCleanUser(foundUser);
-
-      res.status(200).json({
-        success: true,
-        user,
-        token
-      });
-    });
-  });
-});
+router.post('/login', controller.login);
+router.get('/me/from/token', controller.checkToken);
 
 module.exports = router;
