c43c5d69e3c1f20e19429f9ff151851e6cf3e082
Matt <myan5150@gmail.com>
2019-06-29 13:14:58 -0400

    adding auth routes and basic login verification

--- typeserver/routes/auth/index.js
+++ typeserver/routes/auth/index.js
@@ -8,18 +8,62 @@ const { generateToken } = require('./../../utils/auth');
 const User = require('./../../db/model/User');
 
 router.get('/', controller.index);
-router.post('/signup', (req, res, next) => {
-    const { username, email, password } = req.body;
 
-    User({ username, email, password }).save((err, user) => {
-        if (err) {
-            throw err;
-        }
+router.post('/register', (req, res, next) => {
+  const username = req.body.username.trim();
+  const email = req.body.email.trim();
+  const password = bcrypt.hashSync(req.body.password.trim(), 10);
 
-        const token = generateToken(user);
+  User({ username, email, password })
+    .save((err, savedUser) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: err.message });
+      }
 
-        res.status(200).json({ user, token });
+      const token = generateToken(savedUser);
+      const user = {
+        username: savedUser.username,
+        email: savedUser.email
+      };
+
+      res.status(200).json({ success: true, user, token });
+    })
+    .catch(e => {
+      res.status(401).json({ success: false, error: e.message });
     });
-})
+});
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).exec((err, user) => {
+    if (err) {
+      return res.status(401).json({ success: false, message: err.message });
+    }
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Incorrect username or password' });
+    }
+
+    bcrypt.compare(password, user.password, (err, valid) => {
+      if (!valid) {
+        return res.status(404).json({
+          success: false,
+          message: 'Incorrect username or password'
+        });
+      }
+
+      const token = generateToken(user);
+      const formattedUser = {
+        username: user.username,
+        email: user.email
+      };
+
+      res.status(200).json({ success: true, token, user: formattedUser });
+    });
+  });
+});
 
 module.exports = router;
