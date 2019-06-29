941ecb60082288f32b7489d951b55ca104bd849b
Matt <myan5150@gmail.com>
2019-06-29 12:23:50 -0400

    adding bcrypt and initializing logic for user registration via jwt

--- typeserver/routes/auth/index.js
+++ typeserver/routes/auth/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const controller = require('./auth.controller');
+const { generateToken } = require('./../../utils/auth');
 
 const User = require('./../../db/model/User');
 
@@ -9,6 +11,15 @@ router.get('/', controller.index);
 router.post('/signup', (req, res, next) => {
     const { username, email, password } = req.body;
 
+    User({ username, email, password }).save((err, user) => {
+        if (err) {
+            throw err;
+        }
+
+        const token = generateToken(user);
+
+        res.status(200).json({ user, token });
+    });
 })
 
 module.exports = router;
