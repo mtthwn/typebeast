9573e64731b4938e59d48a38bb8ef3d6492d2f6f
Matt <myan5150@gmail.com>
2019-06-29 12:17:09 -0400

    building route for user signup

--- typeserver/routes/auth/index.js
+++ typeserver/routes/auth/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./auth.controller');
 
+const User = require('./../../db/model/User');
+
 router.get('/', controller.index);
+router.post('/signup', (req, res, next) => {
+    const { username, email, password } = req.body;
+
+})
 
 module.exports = router;
