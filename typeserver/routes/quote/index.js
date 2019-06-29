a70d4cfcd2dc669af3ef7ad730573008b72a07c0
Matt <myan5150@gmail.com>
2019-06-29 19:12:45 -0400

    adding quotes controller

--- typeserver/routes/quote/index.js
+++ typeserver/routes/quote/index.js
@@ -3,25 +3,10 @@ const router = express.Router();
 
 const Quote = require('./../../db/model/Quote');
 
-router.get('/', (req, res) => {
-  Quote.find({})
-    .then(quotes => {
-      const index = Math.floor(Math.random() * quotes.length);
+const controller = require('./quote.controller');
 
-      res.status(200).json({ data: quotes[index] });
-    })
-    .catch(e => res.status(400).json({ success: false, message: e.message }));
-});
+router.get('/', controller.getQuote);
 
-router.post('/', (req, res) => {
-  const { author, quote } = req.body;
-
-  new Quote({ author, quote })
-    .save()
-    .then(quote => {
-      res.status(200).json({ quote });
-    })
-    .catch(e => res.status(400).json({ success: false, message: e.message }));
-});
+router.post('/', controller.addQuote);
 
 module.exports = router;
