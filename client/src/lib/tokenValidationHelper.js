c28ebaf8b953c8aede47834d23a96f42e2c68e88
Matt <myan5150@gmail.com>
2019-06-30 17:16:18 -0400

    adding asynchronous loading of state for main page

--- client/src/lib/tokenValidationHelper.js
+++ client/src/lib/tokenValidationHelper.js
@@ -1,35 +1,37 @@
 import axios from 'axios';
 
 export default () => {
-  const token = localStorage.getItem('token');
+  return new Promise((res, rej) => {
+    const token = localStorage.getItem('token');
 
-  if (!token) {
-    return {
-      username: 'Guest',
-      email: null
-    };
-  }
-
-  axios
-    .get(`http://127.0.0.1:8081/api/auth/me/from/token`, {
-      params: {
-        token: JSON.parse(token)
-      }
-    })
-    .then(response => {
-      if (response.data.success) {
-        return response.data.user;
-      }
-
+    if (!token) {
       return {
         username: 'Guest',
-        email: null,
-        _id: null,
-        cars: [],
-        games: []
+        email: null
       };
-    })
-    .catch(e => {
-      console.log(e);
-    });
+    }
+
+    axios
+      .get(`http://127.0.0.1:8081/api/auth/me/from/token`, {
+        params: {
+          token: JSON.parse(token)
+        }
+      })
+      .then(response => {
+        if (response.data.success) {
+          res(response.data.user);
+        }
+
+        rej({
+          username: 'Guest',
+          email: null,
+          _id: null,
+          cars: [],
+          games: []
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  });
 };
