6aedb7212b3348d146e9abaf8f864f8a50c9241c
Daniel <nhanxtrand@gmail.com>
2019-06-30 17:44:10 -0400

    Add additional slide

--- client/src/components/ShopSlider/ShopSlider.js
+++ client/src/components/ShopSlider/ShopSlider.js
@@ -7,8 +7,11 @@ import ImageSlider from '../CarSlider/ImageSlider';
 
 import './ShopSlider.scss';
 
-import silvia from './../../img/nissan_silvia_slide.png';
-import car_sprite1 from './../../img/gtx_md.png';
+import nissan from './../../img/nissan_silvia_slide.png';
+import porsche from './../../img/porsche.png';
+
+import nissan_sprite from './../../img/gtx_md.png';
+import porsche_sprite from './../../img/porsche_md.png';
 
 export default () => {
   return (
@@ -20,13 +23,17 @@ export default () => {
             direction={props.direction}
             onSelect={props.handleSelect}
             indicators={false}
-            fade={true}
+            fade={false}
             controls={true}
             interval="3000"
           >
             <Carousel.Item>
-              <SliderCaption imgSrc={car_sprite1} />
-              <ImageSlider className="CarSlide" src={silvia} />
+              <SliderCaption imgSrc={nissan_sprite} />
+              <ImageSlider className="CarSlide" src={nissan} />
+            </Carousel.Item>
+            <Carousel.Item>
+              <SliderCaption imgSrc={porsche_sprite} />
+              <ImageSlider className="CarSlide" src={porsche} />
             </Carousel.Item>
           </Carousel>
           <Button className="ShopSlider-BuyBtn" variant="outline-light">
