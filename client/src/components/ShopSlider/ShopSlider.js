9f79b6ad56f364d11b4632838b47a3abf3cd9d65
Daniel <nhanxtrand@gmail.com>
2019-06-30 13:36:00 -0400

    Add car spec bars

--- client/src/components/ShopSlider/ShopSlider.js
+++ client/src/components/ShopSlider/ShopSlider.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import CarSliderLogic from '../CarSlider/CarSliderLogic';
-import { Carousel, Button } from 'react-bootstrap';
-
-import {
-  faChevronRight,
-  faChevronLeft
-} from '@fortawesome/free-solid-svg-icons';
+import { Carousel, Button, ProgressBar } from 'react-bootstrap';
 
 import carSlide1 from './../../img/car_slider2.jpg';
 import car_sprite1 from './../../img/gtx.png';
 import './ShopSlider.scss';
 
 export default () => {
+  const maxspeed = 170;
+  const now = maxspeed;
+  const progressInstance = (
+    <ProgressBar now={maxspeed} max="300" label={`${now}mph`} />
+  );
   return (
     <CarSliderLogic>
       {props => (
@@ -28,26 +26,25 @@ export default () => {
             interval="3000"
           >
             <Carousel.Item>
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Carousel.Caption className="CarSlider-content">
+                <h5>Honda</h5>
+                <h3>Civic</h3>
+                <h6>Max Speed</h6>
+                {progressInstance}
+
+                <h6>Acceleration 0-60</h6>
+                {progressInstance}
               </Carousel.Caption>
               <img src={carSlide1} alt="car sprite" />
             </Carousel.Item>
-            <Carousel.Item>
+            {/* <Carousel.Item>
               <img src={carSlide1} alt="car sprite" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
+              <Carousel.Caption className="CarSlider-content">
+                <h5>Nissan</h5>
+                <h3>Skyline GTR</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src={carSlide1} alt="car sprite" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
+            </Carousel.Item> */}
           </Carousel>
           <Button className="ShopSlider-BuyBtn" variant="outline-light">
             Buy Now
