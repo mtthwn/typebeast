b13f0aa3c5d30a73da0bef536d87bbbe640aaabd
Daniel <nhanxtrand@gmail.com>
2019-07-01 09:08:01 -0400

    Add card component

--- client/src/components/Shop/UserInfo.js
+++ client/src/components/Shop/UserInfo.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { CardColumns, Card, Button } from 'react-bootstrap';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faCoins } from '@fortawesome/free-solid-svg-icons';
 
 import './Shop.scss';
 import nissan_sprite from './../../img/gtx_md.png';
+import porsche_sprite from './../../img/porsche.png';
 
 export default () => {
   return (
@@ -28,6 +31,26 @@ export default () => {
             <h3>5000</h3>
           </li>
         </ul>
+      </div>
+
+      <div className="Shop-Carlist-container">
+        <CardColumns>
+          <Card>
+            <Card.Img variant="top" src={porsche_sprite} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button className="Buy-Btn" variant="outline-light">
+                Purchase
+              </Button>{' '}
+            </Card.Footer>
+          </Card>
+        </CardColumns>
       </div>
     </div>
   );
