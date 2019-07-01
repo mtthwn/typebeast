4ca3726d858919013d5333e1d0c432e8fc978b89
Matt <myan5150@gmail.com>
2019-07-01 17:59:38 -0400

    building function to render all cars in the shop

--- client/src/components/Shop/CarList.js
+++ client/src/components/Shop/CarList.js
@@ -3,9 +3,24 @@ import React from 'react';
 import BuyCardColumn from './BuyCardColumn';
 
 export default ({ cars }) => {
-  return (
-    <div className="Shop-Carlist-container">
-      <BuyCardColumn cars={cars} />
-    </div>
-  );
+  const carRows = [];
+
+  let currentRow = [];
+
+  cars.map(car => {
+    currentRow.push(car);
+
+    if (currentRow.length === 3) {
+      carRows.push(currentRow);
+      currentRow = [];
+    }
+  });
+
+  if (currentRow.length > 0) {
+    carRows.push(currentRow);
+  }
+
+  const rows = carRows.map(carRow => <BuyCardColumn cars={carRow} />);
+
+  return <div className="Shop-Carlist-container">{rows}</div>;
 };
