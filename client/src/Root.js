abb4ea48be48830c1e5ea4fa6eb57c88f6de49d2
Daniel <nhanxtrand@gmail.com>
2019-07-01 06:02:23 -0400

    Change shop page to garage

--- client/src/Root.js
+++ client/src/Root.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Main from './containers/Main';
-import Shop from './containers/Shop';
+import Shop from './containers/Garage';
 import Game from './containers/Play';
 
 function Root() {
@@ -12,7 +12,7 @@ function Root() {
     <Router>
       <Header />
       <Route path="/" exact component={Main} />
-      <Route path="/shop" component={Shop} />
+      <Route path="/garage" component={Garage} />
       <Route path="/play" component={Game} />
     </Router>
   );
