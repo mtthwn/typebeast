6245a618cae07680600c70dfa0e9f39f1e519899
Daniel <nhanxtrand@gmail.com>
2019-07-01 12:56:53 -0400

    Remove pricing and instead adding to buy button

--- client/src/components/Shop/BuyCard.js
+++ client/src/components/Shop/BuyCard.js
@@ -12,17 +12,12 @@ export default ({ src }) => {
           <ProgressBar now="177" max="250" label={`177 mph`} />
           <h6>Acceleration 0-100 kph</h6>
           <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
-          <h6>Price</h6>
-          <div className="Shop-CarPrice">
-            <h1>$5000</h1>
-            <h5 className="coins">Coins</h5>
-          </div>
         </Card.Text>
       </Card.Body>
       <Card.Footer>
         <Button className="Buy-Btn" variant="outline-light">
-          Purchase
-        </Button>{' '}
+          $5000
+        </Button>
       </Card.Footer>
     </Card>
   );
