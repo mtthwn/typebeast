be0e65de0b68af1accca23b3af363c64ca389685
Matt <myan5150@gmail.com>
2019-07-01 17:46:45 -0400

    adding dynamic rendering of shop cars from mongoose data

--- client/src/components/Shop/BuyCard.js
+++ client/src/components/Shop/BuyCard.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import { Card, Button, ProgressBar } from 'react-bootstrap';
+import nissan_sprite from './../../img/gtx_md.png';
+import porsche_sprite from './../../img/porsche_md.png';
 
-export default ({ src }) => {
-  return (
-    <Card>
-      <Card.Img variant="top" src={src} />
-      <Card.Body>
-        <Card.Title>Nissan SILVIA S15</Card.Title>
-        <Card.Text>
-          <h6>Max Speed</h6>
-          <ProgressBar now="177" max="250" label={`177 mph`} />
-          <h6>Acceleration 0-100 kph</h6>
-          <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <Button className="Buy-Btn" variant="outline-light">
-          $5000
-        </Button>
-      </Card.Footer>
-    </Card>
-  );
-};
-
-// export default props => {
-//   console.log(props);
+// export default ({ src }) => {
 //   return (
 //     <Card>
-//       <Card.Img variant="top" />
+//       <Card.Img variant="top" src={src} />
 //       <Card.Body>
 //         <Card.Title>Nissan SILVIA S15</Card.Title>
 //         <Card.Text>
@@ -45,3 +24,35 @@ export default ({ src }) => {
 //     </Card>
 //   );
 // };
+
+const getSprite = (make, model) => {
+  if (make === 'Nissan') {
+    return nissan_sprite;
+  } else if (make === 'Porsche') {
+    return porsche_sprite;
+  }
+}
+
+export default ({ car }) => {
+  const imageImport = getSprite(car.make, car.model);
+  
+  return (
+    <Card>
+      <Card.Img variant="top" src={imageImport} />
+      <Card.Body>
+        <Card.Title>{car.make} {car.model}</Card.Title>
+        <Card.Text>
+          <h6>Max Speed</h6>
+          <ProgressBar now="177" max="250" label={`177 mph`} />
+          <h6>Acceleration 0-100 kph</h6>
+          <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <Button className="Buy-Btn" variant="outline-light">
+          ${car.price}
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
+};
--- client/src/components/Shop/BuyCardColumn.js
+++ client/src/components/Shop/BuyCardColumn.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { CardColumns } from 'react-bootstrap';
 
 import BuyCard from './BuyCard';
-import nissan_sprite from './../../img/gtx_md.png';
-import porsche_sprite from './../../img/porsche_md.png';
 
-export default props => {
-  console.log('here', props);
-  // const renderCars = cars.map(car => <BuyCard car={car} />);
+export default ({ cars }) => {
+  const renderCars = cars.map(car => <BuyCard car={car} />);
   return (
     <CardColumns>
-      <BuyCard src={nissan_sprite} />
-      <BuyCard src={porsche_sprite} />
-      <BuyCard src={porsche_sprite} />
+      {renderCars}
     </CardColumns>
   );
 };
