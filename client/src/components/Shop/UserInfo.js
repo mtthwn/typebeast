2993683754384fdbdffbc5fc5c2e0af7897ae724
Daniel <nhanxtrand@gmail.com>
2019-07-01 06:51:28 -0400

    Add interface for user info

--- client/src/components/Shop/UserInfo.js
+++ client/src/components/Shop/UserInfo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Shop.scss';
+import nissan_sprite from './../../img/gtx_md.png';
 
 export default () => {
   return (
     <div className="Shop-container">
       <div className="CarSlider-content UserInfo">
+        <img className="Car-Sprite" src={nissan_sprite} alt="car sprite" />
         <h5>User</h5>
         <h2>Big Kwok</h2>
         <h5>Coins</h5>
