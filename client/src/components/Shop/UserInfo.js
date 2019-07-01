c905c6ddd98e49dcee1a925782a5b26ba6809577
Daniel <nhanxtrand@gmail.com>
2019-07-01 10:45:48 -0400

    Add hover card animations

--- client/src/components/Shop/UserInfo.js
+++ client/src/components/Shop/UserInfo.js
@@ -104,6 +104,74 @@ export default () => {
             </Card.Footer>
           </Card>
         </CardColumns>
+        <CardColumns>
+          <Card>
+            <Card.Img variant="top" src={porsche_sprite} />
+            <Card.Body>
+              <Card.Title>Nissan SILVIA S15</Card.Title>
+              <Card.Text>
+                <h6>Max Speed</h6>
+                <ProgressBar now="177" max="250" label={`177 mph`} />
+                <h6>Acceleration 0-100 kph</h6>
+                <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
+                <h6>Price</h6>
+                <div className="Shop-CarPrice">
+                  <h1>$5000</h1>
+                  <h5 className="coins">Coins</h5>
+                </div>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button className="Buy-Btn" variant="outline-light">
+                Purchase
+              </Button>{' '}
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={porsche_sprite} />
+            <Card.Body>
+              <Card.Title>Nissan SILVIA S15</Card.Title>
+              <Card.Text>
+                <h6>Max Speed</h6>
+                <ProgressBar now="177" max="250" label={`177 mph`} />
+                <h6>Acceleration 0-100 kph</h6>
+                <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
+                <h6>Price</h6>
+                <div className="Shop-CarPrice">
+                  <h1>$5000</h1>
+                  <h5 className="coins">Coins</h5>
+                </div>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button className="Buy-Btn" variant="outline-light">
+                Purchase
+              </Button>{' '}
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={porsche_sprite} />
+            <Card.Body>
+              <Card.Title>Nissan SILVIA S15</Card.Title>
+              <Card.Text>
+                <h6>Max Speed</h6>
+                <ProgressBar now="177" max="250" label={`177 mph`} />
+                <h6>Acceleration 0-100 kph</h6>
+                <ProgressBar now="5.5" max="10" label={`5.5 seconds`} />
+                <h6>Price</h6>
+                <div className="Shop-CarPrice">
+                  <h1>$5000</h1>
+                  <h5 className="coins">Coins</h5>
+                </div>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button className="Buy-Btn" variant="outline-light">
+                Purchase
+              </Button>{' '}
+            </Card.Footer>
+          </Card>
+        </CardColumns>
       </div>
     </div>
   );
