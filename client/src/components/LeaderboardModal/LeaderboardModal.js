43d72dc83b8510e4fc261691925977a89e1be2ee
Daniel <nhanxtrand@gmail.com>
2019-07-02 18:22:27 -0400

    Remove uneccessary import

--- client/src/components/LeaderboardModal/LeaderboardModal.js
+++ client/src/components/LeaderboardModal/LeaderboardModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Spinner, Modal, Table } from 'react-bootstrap';
 import './LeaderboardModal.scss';
 import raceResultsIcon from './../../img/result-icon.png';
