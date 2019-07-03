a8599adecf895c3cad931fc4a2f19c52c0bcc98e
Daniel <nhanxtrand@gmail.com>
2019-07-02 23:06:28 -0400

    Move button styles to main styles folder

--- client/src/components/Button/HeaderButton.js
+++ client/src/components/Button/HeaderButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import './Button.scss';
+import '../../styles/Buttons.scss';
 
 export default ({ className, buttonText, cb }) => (
   <Button onClick={cb ? cb : undefined} className={className} variant="primary">
