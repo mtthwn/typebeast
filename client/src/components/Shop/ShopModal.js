a51aa802158f5aff7f00d87e66ad44bc35f4f3b4
Daniel <nhanxtrand@gmail.com>
2019-07-03 09:14:06 -0400

    Add success header on purchase

--- client/src/components/Shop/ShopModal.js
+++ client/src/components/Shop/ShopModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Badge } from 'react-bootstrap';
 
 import purchaseSuccessful from './../../img/success.png';
 
@@ -8,15 +8,19 @@ export default ({ show }) => {
   return (
     <div className="Shop-Modal">
       <Modal show={show} dialogClassName="modal-100w" centered>
+        <Modal.Title className="ShopModal-title">
+          Purchase Successful
+        </Modal.Title>
         <Modal.Body>
           <div className="Result-wrapper">
             <img src={purchaseSuccessful} />
+            {/* <Badge variant="success">Purchase Successful!</Badge> */}
           </div>
 
           <Button variant="none" href="/" className="PlayAgain-btn">
             Home
           </Button>
-          <Link to={'/'}>
+          <Link to={'/shop'}>
             <Button variant="none" className="Home-btn">
               Keep Shopping
             </Button>
