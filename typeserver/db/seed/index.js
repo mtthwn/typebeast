f59d7f7f0d98e3457c7954de6488d73a7c7e8d9c
Matt <myan5150@gmail.com>
2019-07-03 22:22:04 -0400

    moving seed data to separate files

--- typeserver/db/seed/index.js
+++ typeserver/db/seed/index.js
@@ -7,89 +7,12 @@ const Game = require('./../model/Game');
 const User = require('./../model/User');
 
 const seed = require('./quotes');
+const cars = require('./cars');
+const users = require('./users');
 
 let counter = 0;
 
 const collections = ['games', 'quotes', 'cars', 'users'];
-
-const users = [
-  {
-    username: 'Justin',
-    email: 'jskwok@gmail.com',
-    games: [],
-    password: bcrypt.hashSync('hello', 10)
-  },
-  {
-    username: 'Daniel',
-    email: 'dtran23@gmail.com',
-    games: [],
-    password: bcrypt.hashSync('goodbye', 10)
-  }
-];
-
-
-const cars = [
-  {
-    model: 'Silvia S15',
-    make: 'Nissan',
-    spriteFile: 'https://i.imgur.com/a4hVyfy.png',
-    mediumImg: 'https://i.imgur.com/nJYJFty.png',
-    largeImg: 'https://i.imgur.com/Evae8Vc.png',
-    price: 5000,
-    maxSpeed: 170,
-    acceleration: 5.5
-  },
-  {
-    model: '935',
-    make: 'Porsche',
-    spriteFile: 'https://i.imgur.com/UBAaAZq.png',
-    mediumImg: 'https://i.imgur.com/8k3z73p.png',
-    largeImg: 'https://i.imgur.com/9CkPCCn.png',
-    price: 5000,
-    maxSpeed: 183,
-    acceleration: 5
-  },
-  {
-    model: 'Chillvia',
-    make: 'Nissan',
-    spriteFile: 'https://i.imgur.com/a4hVyfy.png',
-    mediumImg: 'https://i.imgur.com/nJYJFty.png',
-    largeImg: 'https://i.imgur.com/Evae8Vc.png',
-    price: 5000,
-    maxSpeed: 150,
-    acceleration: 7
-  },
-  {
-    model: 'Horse',
-    make: 'Lil Nas X',
-    spriteFile: 'https://i.imgur.com/UBAaAZq.png',
-    mediumImg: 'https://i.imgur.com/8k3z73p.png',
-    largeImg: 'https://i.imgur.com/9CkPCCn.png',
-    price: 5000,
-    maxSpeed: 50,
-    acceleration: 3
-  },
-  {
-    model: 'Not-Civic',
-    make: 'Honda',
-    spriteFile: 'https://i.imgur.com/a4hVyfy.png',
-    mediumImg: 'https://i.imgur.com/nJYJFty.png',
-    largeImg: 'https://i.imgur.com/Evae8Vc.png',
-    price: 5000,
-    maxSpeed: 170,
-    acceleration: 6.9
-  },
-  {
-    model: 'Model S',
-    make: 'Tesla',
-    spriteFile: 'https://i.imgur.com/UBAaAZq.png',
-    mediumImg: 'https://i.imgur.com/8k3z73p.png',
-    largeImg: 'https://i.imgur.com/9CkPCCn.png',
-    price: 5000,
-    maxSpeed: 250,
-    acceleration: 3.2
-  }
-];
 
 const seedQuotes = async quotes => {
   collections.forEach(collection => {
