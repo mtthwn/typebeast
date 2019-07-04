0b05364e81d42fb80d258333169b92dd2dd81614
Matt <myan5150@gmail.com>
2019-07-03 22:19:57 -0400

    adding separate file for quotes seed data

--- typeserver/db/seed/index.js
+++ typeserver/db/seed/index.js
@@ -6,6 +6,8 @@ const Car = require('./../model/Car');
 const Game = require('./../model/Game');
 const User = require('./../model/User');
 
+const seed = require('./quotes');
+
 let counter = 0;
 
 const collections = ['games', 'quotes', 'cars', 'users'];
@@ -25,39 +27,6 @@ const users = [
   }
 ];
 
-const seed = [
-  {
-    author: 'Henry David Thoreau',
-    quote:
-      'Do not worry if you have built your castles in the air. They are where they should be. Now put the foundations under them.'
-  },
-  {
-    author: 'George Bernard Shaw',
-    quote:
-      'Reasonable people adapt themselves to the world. Unreasonable people attempt to adapt the world to themselves. All progress, therefore, depends on unreasonable people.'
-  },
-  {
-    author: 'Albert Einstein',
-    quote: `Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`
-  },
-  {
-    quote: `Time may restore us in his course Goethe's sage mind and Byron's force: But where will Europe's latter hour Again find Wordsworth's healing power?`,
-    author: 'Matthew Arnold'
-  },
-  {
-    quote: `When I have a bad day, I dream about opening up a gelato stand on the streets of Sydney, Australia. Doesn't everyone have a random escape fantasy?`,
-    author: 'Nancy Lublin'
-  },
-  {
-    quote:
-      'We can all fight against loneliness by engaging in random acts of kindness.',
-    author: 'Gail Honeyman'
-  },
-  {
-    quote: `When life offers you a dream so far beyond any of your expectations, it's not reasonable to grieve when it comes to an end.`,
-    author: 'Stephanie Meyer'
-  }
-];
 
 const cars = [
   {
---
+++ typeserver/db/seed/quotes.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    author: 'Henry David Thoreau',
+    quote:
+      'Do not worry if you have built your castles in the air. They are where they should be. Now put the foundations under them.'
+  },
+  {
+    author: 'George Bernard Shaw',
+    quote:
+      'Reasonable people adapt themselves to the world. Unreasonable people attempt to adapt the world to themselves. All progress, therefore, depends on unreasonable people.'
+  },
+  {
+    author: 'Albert Einstein',
+    quote: `Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`
+  },
+  {
+    quote: `Time may restore us in his course Goethe's sage mind and Byron's force: But where will Europe's latter hour Again find Wordsworth's healing power?`,
+    author: 'Matthew Arnold'
+  },
+  {
+    quote: `When I have a bad day, I dream about opening up a gelato stand on the streets of Sydney, Australia. Doesn't everyone have a random escape fantasy?`,
+    author: 'Nancy Lublin'
+  },
+  {
+    quote:
+      'We can all fight against loneliness by engaging in random acts of kindness.',
+    author: 'Gail Honeyman'
+  },
+  {
+    quote: `When life offers you a dream so far beyond any of your expectations, it's not reasonable to grieve when it comes to an end.`,
+    author: 'Stephanie Meyer'
+  }
+];
