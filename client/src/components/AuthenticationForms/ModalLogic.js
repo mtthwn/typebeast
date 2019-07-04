0d6f5b0f741e733bd023bdc2d9e85baa7ee73bf3
Matt <myan5150@gmail.com>
2019-07-04 08:31:27 -0400

    moving seed data location and adjusting reloading of window after login

--- client/src/components/AuthenticationForms/ModalLogic.js
+++ client/src/components/AuthenticationForms/ModalLogic.js
@@ -19,41 +19,43 @@ export default class ModalLogic extends Component {
 
   handleLogin = (email, password) => {
     // e.preventDefault();
-    axios.post('http://127.0.0.1:8081/api/auth/login', {
-      email,
-      password
-    })
-    .then((res) => {
-      // console.log('Before saving to storage', res.data.token)
-      const token = res.data.token
-      localStorage.setItem('token', JSON.stringify(token));
-      // console.log('Retrieve from storage', localStorage.getItem('token'))
-    }).then(() => {
-      window.location.reload()
-    })
-    .catch(e => {
-      console.log(e.message)
-    })
-  }
+    axios
+      .post('http://127.0.0.1:8081/api/auth/login', {
+        email,
+        password
+      })
+      .then(res => {
+        // console.log('Before saving to storage', res.data.token)
+        const token = res.data.token;
+        localStorage.setItem('token', JSON.stringify(token));
+        window.location.reload();
+
+        // console.log('Retrieve from storage', localStorage.getItem('token'))
+      })
+      .catch(e => {
+        console.log(e.message);
+      });
+  };
 
   handleRegister = (username, email, password) => {
-    axios.post('http://127.0.0.1:8081/api/auth/register', {
-      username,
-      email,
-      password
-    })
-    .then((res) => {
-      const token = res.data.token
-      localStorage.setItem('token', JSON.stringify(token));
-      console.log('Retrieve from storage', localStorage.getItem('token'))
-    })
-    .then(() => {
-      window.location.reload()
-    })
-    .catch(e => {
-      console.log(e.message)
-    })
-  }
+    axios
+      .post('http://127.0.0.1:8081/api/auth/register', {
+        username,
+        email,
+        password
+      })
+      .then(res => {
+        const token = res.data.token;
+        localStorage.setItem('token', JSON.stringify(token));
+        console.log('Retrieve from storage', localStorage.getItem('token'));
+      })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch(e => {
+        console.log(e.message);
+      });
+  };
 
   render() {
     return this.props.children({
--- typeserver/db/seed/index.js
+++ typeserver/db/seed/index.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const { mongoose, db } = require('./../config');
 const Quote = require('./../model/Quote');
 const Car = require('./../model/Car');
@@ -12,118 +14,118 @@ let counter = 0;
 
 const collections = ['games', 'quotes', 'cars', 'users'];
 
-const users = [
-  {
-    username: 'Justin',
-    email: 'jskwok@gmail.com',
-    games: [],
-    password: bcrypt.hashSync('hello', 10)
-  },
-  {
-    username: 'Daniel',
-    email: 'dtran23@gmail.com',
-    games: [],
-    password: bcrypt.hashSync('goodbye', 10)
-  }
-];
+// const users = [
+//   {
+//     username: 'Justin',
+//     email: 'jskwok@gmail.com',
+//     games: [],
+//     password: bcrypt.hashSync('hello', 10)
+//   },
+//   {
+//     username: 'Daniel',
+//     email: 'dtran23@gmail.com',
+//     games: [],
+//     password: bcrypt.hashSync('goodbye', 10)
+//   }
+// ];
 
-const seed = [
-  {
-    author: 'Henry David Thoreau',
-    quote:
-      'Do not worry if you have built your castles in the air. They are where they should be. Now put the foundations under them.'
-  },
-  {
-    author: 'Lorii Myers',
-    quote:
-      'People rise to the occasion without hesitation when they feel inspired and challenged.'
-  },
-  {
-    author: 'Colin Powell',
-    quote:
-      'The day soldiers stop bringing you their problems is the day you have stopped leading them. They have either lost confidence that you can help them or concluded that you do not care. Either case is a failure of leadership.'
-  },
-  {
-    quote: `I don't believe that if you do good, good things will happen. Everything is completely accidental and random. Sometimes bad things happen to very good people and sometimes good things happen to bad people. But at least if you try to do good things, then you're spending your time doing something worthwhile.`,
-    author: 'Helen Mirren'
-  },
-  {
-    quote: `When I have a bad day, I dream about opening up a gelato stand on the streets of Sydney, Australia. Doesn't everyone have a random escape fantasy?`,
-    author: 'Nancy Lublin'
-  },
-  {
-    quote:
-      'We can all fight against loneliness by engaging in random acts of kindness.',
-    author: 'Gail Honeyman'
-  },
-  {
-    quote: `When life offers you a dream so far beyond any of your expectations, it's not reasonable to grieve when it comes to an end.`,
-    author: 'Stephanie Meyer'
-  }
-];
+// const seed = [
+//   {
+//     author: 'Henry David Thoreau',
+//     quote:
+//       'Do not worry if you have built your castles in the air. They are where they should be. Now put the foundations under them.'
+//   },
+//   {
+//     author: 'Lorii Myers',
+//     quote:
+//       'People rise to the occasion without hesitation when they feel inspired and challenged.'
+//   },
+//   {
+//     author: 'Colin Powell',
+//     quote:
+//       'The day soldiers stop bringing you their problems is the day you have stopped leading them. They have either lost confidence that you can help them or concluded that you do not care. Either case is a failure of leadership.'
+//   },
+//   {
+//     quote: `I don't believe that if you do good, good things will happen. Everything is completely accidental and random. Sometimes bad things happen to very good people and sometimes good things happen to bad people. But at least if you try to do good things, then you're spending your time doing something worthwhile.`,
+//     author: 'Helen Mirren'
+//   },
+//   {
+//     quote: `When I have a bad day, I dream about opening up a gelato stand on the streets of Sydney, Australia. Doesn't everyone have a random escape fantasy?`,
+//     author: 'Nancy Lublin'
+//   },
+//   {
+//     quote:
+//       'We can all fight against loneliness by engaging in random acts of kindness.',
+//     author: 'Gail Honeyman'
+//   },
+//   {
+//     quote: `When life offers you a dream so far beyond any of your expectations, it's not reasonable to grieve when it comes to an end.`,
+//     author: 'Stephanie Meyer'
+//   }
+// ];
 
-const cars = [
-  {
-    model: 'Silvia S15',
-    make: 'Nissan',
-    spriteFile: 'https://i.imgur.com/a4hVyfy.png',
-    mediumImg: 'https://i.imgur.com/nJYJFty.png',
-    largeImg: 'https://i.imgur.com/Evae8Vc.png',
-    price: 10000,
-    maxSpeed: 170,
-    acceleration: 5.5
-  },
-  {
-    model: '935',
-    make: 'Porsche',
-    spriteFile: 'https://i.imgur.com/UBAaAZq.png',
-    mediumImg: 'https://i.imgur.com/8k3z73p.png',
-    largeImg: 'https://i.imgur.com/9CkPCCn.png',
-    price: 15000,
-    maxSpeed: 183,
-    acceleration: 5
-  },
-  {
-    model: 'Ford',
-    make: 'Mustang GT 67',
-    spriteFile: 'https://i.imgur.com/nlH4JEk.png',
-    mediumImg: 'https://i.imgur.com/7D2UQfN.png',
-    largeImg: 'https://i.imgur.com/kYmO4vg.png',
-    price: 5000,
-    maxSpeed: 150,
-    acceleration: 7
-  },
-  {
-    model: 'Horse',
-    make: 'Lil Nas X',
-    spriteFile: 'https://i.imgur.com/PhTiQyf.png',
-    mediumImg: 'https://i.imgur.com/Soc0Qx9.png',
-    largeImg: 'https://i.imgur.com/GKwJplQ.png',
-    price: 1000,
-    maxSpeed: 50,
-    acceleration: 3
-  },
-  {
-    model: 'McLaren',
-    make: 'P1',
-    spriteFile: 'https://i.imgur.com/HXrjtI3.png',
-    mediumImg: 'https://i.imgur.com/ZyFon8s.png',
-    largeImg: 'https://i.imgur.com/N5ZzKJU.png',
-    price: 2000,
-    maxSpeed: 200,
-    acceleration: 4.9
-  },
-  {
-    model: 'Model S',
-    make: 'Tesla',
-    spriteFile: 'https://i.imgur.com/3r9vmlE.png',
-    mediumImg: 'https://i.imgur.com/iZoD5SZ.png',
-    largeImg: 'https://i.imgur.com/h9qFaib.png',
-    price: 5000,
-    maxSpeed: 250,
-    acceleration: 3.2
-  }
-];
+// const cars = [
+//   {
+//     model: 'Silvia S15',
+//     make: 'Nissan',
+//     spriteFile: 'https://i.imgur.com/a4hVyfy.png',
+//     mediumImg: 'https://i.imgur.com/nJYJFty.png',
+//     largeImg: 'https://i.imgur.com/Evae8Vc.png',
+//     price: 10000,
+//     maxSpeed: 170,
+//     acceleration: 5.5
+//   },
+//   {
+//     model: '935',
+//     make: 'Porsche',
+//     spriteFile: 'https://i.imgur.com/UBAaAZq.png',
+//     mediumImg: 'https://i.imgur.com/8k3z73p.png',
+//     largeImg: 'https://i.imgur.com/9CkPCCn.png',
+//     price: 15000,
+//     maxSpeed: 183,
+//     acceleration: 5
+//   },
+//   {
+//     model: 'Ford',
+//     make: 'Mustang GT 67',
+//     spriteFile: 'https://i.imgur.com/nlH4JEk.png',
+//     mediumImg: 'https://i.imgur.com/7D2UQfN.png',
+//     largeImg: 'https://i.imgur.com/kYmO4vg.png',
+//     price: 5000,
+//     maxSpeed: 150,
+//     acceleration: 7
+//   },
+//   {
+//     model: 'Horse',
+//     make: 'Lil Nas X',
+//     spriteFile: 'https://i.imgur.com/PhTiQyf.png',
+//     mediumImg: 'https://i.imgur.com/Soc0Qx9.png',
+//     largeImg: 'https://i.imgur.com/GKwJplQ.png',
+//     price: 1000,
+//     maxSpeed: 50,
+//     acceleration: 3
+//   },
+//   {
+//     model: 'McLaren',
+//     make: 'P1',
+//     spriteFile: 'https://i.imgur.com/HXrjtI3.png',
+//     mediumImg: 'https://i.imgur.com/ZyFon8s.png',
+//     largeImg: 'https://i.imgur.com/N5ZzKJU.png',
+//     price: 2000,
+//     maxSpeed: 200,
+//     acceleration: 4.9
+//   },
+//   {
+//     model: 'Model S',
+//     make: 'Tesla',
+//     spriteFile: 'https://i.imgur.com/3r9vmlE.png',
+//     mediumImg: 'https://i.imgur.com/iZoD5SZ.png',
+//     largeImg: 'https://i.imgur.com/h9qFaib.png',
+//     price: 5000,
+//     maxSpeed: 250,
+//     acceleration: 3.2
+//   }
+// ];
 
 const seedQuotes = async quotes => {
   collections.forEach(collection => {
