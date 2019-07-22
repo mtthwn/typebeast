21898b9fd893af428bd48a2263477148ab6e94f5
Matt <myan5150@gmail.com>
2019-07-22 17:29:14 -0400

    building out new page for hooks in main

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,21 +1,17 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import './Root.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Garage, Game, Main, Shop } from './containers';
 
-import UserContext from './context/user-context';
-
 function Root() {
   return (
-    <UserContext.Provider>
       <Router>
         <Route path="/" exact component={Main} />
         <Route path="/garage" component={Garage} />
         <Route path="/shop" component={Shop} />
         <Route path="/play" component={Game} />
       </Router>
-    </UserContext.Provider>
   );
 }
 
--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,7 +1,14 @@
 const usersReducer = (state, action) => {
   switch (action.type) {
     case 'UPDATE_USER':
-      return { ...action.user };
+      return action.user;
+    case 'LOGOUT_USER':
+      return {
+        username: '',
+        cars: [],
+        email: null,
+        games: []
+      };
     default:
       return state;
   }
