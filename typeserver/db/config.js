ca997f1c0597bf62f6f4188f706904fefd1211ab
Matt <myan5150@gmail.com>
2019-08-07 19:59:20 -0400

    adding dockerignore file

--- typeserver/db/config.js
+++ typeserver/db/config.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+
 mongoose.connect('mongodb://localhost:27017/typebeast', {
   useNewUrlParser: true,
   useFindAndModify: false
